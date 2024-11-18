0af4ef7348b6463b252ac5ffdf76cf0bf0311bac
Aayush <just.another.3d.object@gmail.com>
2024-11-18 13:02:54 +0530

    Added commits for HSV_filter

--- HSV_filter.py
+++ HSV_filter.py
@@ -5,15 +5,18 @@ import time
 
 def add_HSV_filter(frame):
 
+    #Blurring the frame
     blur = cv2.GaussianBlur(frame,(5,5),0)
 
+    #Converting RGB to HSV
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    lower_blue = np.array([0, 100, 100]) 
-    upper_blue = np.array([179, 255, 255])
+    lower_blue = np.array([0, 100, 100])    #Lower limit for red colour in HSV
+    upper_blue = np.array([179, 255, 255])  #Upper limit for red colour in HSV
  
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     
+    #Morphological operations: Erode followed by Dilate to remove noise
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
